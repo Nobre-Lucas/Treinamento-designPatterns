d47d55d29db9a74a5e0b1d8f3195407d6eea07bf
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2023-03-27 08:57:18 -0300

    Implement Proxy Design Pattern

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -21,6 +21,11 @@ public class Orcamento implements Orcavel {
 	}
 
 	public BigDecimal getValor() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return valor;
 	}
 
